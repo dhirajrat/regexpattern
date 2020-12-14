a12ede6d035984bbb938cea087c5f9913da22513
dhirajrathod <dhirajrathod3000@gmail.com>
2020-12-14 19:38:00 +0530

    merging from UC3

--- regex.sh
+++ regex.sh
@@ -26,3 +26,16 @@ else
 	echo "invalid"
 fi
 done
+
+while [[ flag -eq 0 ]]
+do
+read -p "enter mobile number with country code check validate" input
+pattern="^[0-9]{2}[ ][0-9]{10}$"
+if [[ $input =~ $pattern ]]
+then
+        echo "valid"
+        flag=1
+else
+        echo "invalid"
+fi
+done
