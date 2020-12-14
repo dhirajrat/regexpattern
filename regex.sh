597de0ec4348dd6fc31e70e2bac2d9776ed15f3c
dhirajrathod <dhirajrathod3000@gmail.com>
2020-12-14 19:58:25 +0530

    add to UC4

--- regex.sh
+++ regex.sh
@@ -30,11 +30,25 @@ done
 while [[ flag -eq 0 ]]
 do
 read -p "enter mobile number with country code check validate" input
-pattern="^[0-9]{2}[ ][0-9]{10}$"
+pattern="^[0-9]{2}[ ][0-9]{10}"
 if [[ $input =~ $pattern ]]
 then
         echo "valid"
         flag=1
+else
+        echo "invalid"
+fi
+done
+
+
+while [[ flag -eq 1 ]]
+do
+read -p "enter email check validate" input
+pattern="^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})*$"
+if [[ $input =~ $pattern ]]
+then
+        echo "valid"
+        flag=0
 else
         echo "invalid"
 fi
