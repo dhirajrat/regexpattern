03b715aaf7e459347c12bf17235016a18af25e7f
dhirajrathod <dhirajrathod3000@gmail.com>
2020-12-14 19:28:49 +0530

    add UC2

--- regex.sh
+++ regex.sh
@@ -1,10 +1,28 @@
-#!/bin/bash -x
+#!/bin/bash
+flag=0
 
-read -p "enter word check validate" input
+while [[ flag -eq 0 ]]
+do
+read -p "enter first name check validate" input
 pattern="^[A-Z][a-zA-Z]{2,}$"
 if [[ $input =~ $pattern ]]
 then
-	echo "valid"
+        echo "valid"
+	flag=1
 else
 	echo "invalid"
 fi
+done
+
+while [[ flag -eq 1 ]]
+do
+read -p "enter last name check validate" input
+pattern="^[A-Z][a-zA-Z]{2,}$"
+if [[ $input =~ $pattern ]]
+then
+        echo "valid"
+	flag=0
+else
+	echo "invalid"
+fi
+done
